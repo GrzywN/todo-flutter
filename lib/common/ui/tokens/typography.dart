8589040af444426e662c6574e9b202f59a0b3273
Karol Binkowski <starystachu9@gmail.com>
2023-08-04 13:54:01 +0200

    refactor: extract fontFamily name to Typography UI token

---
+++ lib/common/ui/tokens/typography.dart
@@ -0,0 +1,3 @@
+class Typography {
+  static const String fontFamilySans = 'Roboto';
+}
--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/common/ui/tokens/typography.dart' as my_typography;
 
 ThemeData theme() {
   return ThemeData(
     primarySwatch: Colors.indigo,
     useMaterial3: true,
-    fontFamily: 'Roboto',
+    fontFamily: my_typography.Typography.fontFamilySans,
   );
 }
