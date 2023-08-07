be4be84c8bd58ae39206743036690fa69fbca15e
Karol Binkowski <karolbinkowski3@proton.me>
2023-08-07 13:16:46 +0200

    fix: make list sorted on app init

--- lib/features/todo/presentation/bloc/local/local_todo_bloc.dart
+++ lib/features/todo/presentation/bloc/local/local_todo_bloc.dart
@@ -32,35 +32,37 @@ class LocalTodoBloc extends Bloc<LocalTodoEvent, LocalTodoState> {
     todos.sort((a, b) => a.isCompleted == b.isCompleted ? 0 : (a.isCompleted ?? false ? 1 : -1));
   }
 
-  void onAddTodo(AddTodo event, Emitter<LocalTodoState> emit) async {
+  Future<void> onAddTodo(AddTodo event, Emitter<LocalTodoState> emit) async {
     await _addTodoUseCase(params: event.todo);
     final todos = await _getTodosUseCase();
     _sortByIsCompleted(todos);
     emit(LocalTodoDone(todos));
   }
 
-  void onCompleteTodo(CompleteTodo event, Emitter<LocalTodoState> emit) async {
+  Future<void> onCompleteTodo(CompleteTodo event, Emitter<LocalTodoState> emit) async {
     await _completeTodoUseCase(params: event.todo);
     final todos = await _getTodosUseCase();
     _sortByIsCompleted(todos);
     emit(LocalTodoDone(todos));
   }
 
-  void onDeleteTodo(DeleteTodo event, Emitter<LocalTodoState> emit) async {
+  Future<void> onDeleteTodo(DeleteTodo event, Emitter<LocalTodoState> emit) async {
     await _deleteTodoUseCase(params: event.todo);
     final todos = await _getTodosUseCase();
+    _sortByIsCompleted(todos);
     emit(LocalTodoDone(todos));
   }
 
-  void onEditTodo(EditTodo event, Emitter<LocalTodoState> emit) async {
+  Future<void> onEditTodo(EditTodo event, Emitter<LocalTodoState> emit) async {
     await _editTodoUseCase(params: event.todo);
     final todos = await _getTodosUseCase();
     _sortByIsCompleted(todos);
     emit(LocalTodoDone(todos));
   }
 
-  void onGetTodos(GetTodos event, Emitter<LocalTodoState> emit) async {
+  Future<void> onGetTodos(GetTodos event, Emitter<LocalTodoState> emit) async {
     final todos = await _getTodosUseCase();
+    _sortByIsCompleted(todos);
     emit(LocalTodoDone(todos));
   }
 }
